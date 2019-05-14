5cf8a604e4e310fc2b1c94e2025c95d6eb7de98c
dramosv90 <vdariel90@gmail.com>
2019-05-14 16:16:54 -0400

    Adding font awesome and using library to register somes icons

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCode, faHome } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+library.add(faCode, faHome)
 
 const App = () => (
     <div className="container">
         <h1>Hello World, from React App!!</h1>
-        <button className="btn btn-info">Okey</button>
+        <button className="btn btn-info"><FontAwesomeIcon icon="home" /> Okey</button>
     </div>
 )
 

245181ad4bd0fc7b221f1868dd6b42de78c5e61e
dramosv90 <dariel.ramos.dv@gmail.com>
2019-05-19 10:47:49 -0400

    adding card to see how implement ToDoCard component

--- src/components/App.js
+++ src/components/App.js
@@ -12,6 +12,18 @@ const App = () => (
         <button className="btn btn-info"><FontAwesomeIcon icon="home" /> Okey</button>
         <br />
         <TodoItem title="Probando un todoItem" strikeIt={true} color={{ done: '#ed1c24', default: '#ccc' }} />
+
+        <div className="card bg-default" style={{ width: 300 }}>
+            <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <h6 class="card-subtitle text-muted float-right">Updated 2 min ago</h6>
+            </div>
+            <div className="card-footer">
+                <a href="#" className="btn btn-xs btn-success">Add Task</a>
+                <a href="#" className="btn btn-xs btn-danger float-right">Remove</a>
+            </div>
+        </div>
     </div>
 )
 

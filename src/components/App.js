e5a1c73f0f601363c7627e38a829375ffcf185f7
dramosv90 <dariel.ramos.dv@gmail.com>
2019-05-19 13:28:47 -0400

    adding destructuring to access state data and remove card with animatecss effect

--- src/components/App.js
+++ src/components/App.js
@@ -11,10 +11,12 @@ class App extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            addTaskClassName: ["btn", "btn-sm", "btn-success"]
+            addTaskClassName: "btn btn-sm btn-success",
+            cardCss: "card bg-default animated bounceIn mt-5"
         }
 
         this.addTask = this.addTask.bind(this)
+        this.removeCard = this.removeCard.bind(this)
     }
 
     componentWillMount() {
@@ -22,12 +24,21 @@ class App extends React.Component {
     }
 
     addTask() {
-        this.setState((state) => {
-            return { addTaskClassName: state.addTaskClassName.replaceClass("btn-success", "btn-warning") }
+        this.setState(state => {
+            return { addTaskClassName: state.addTaskClassName.split(' ').replaceClass("btn-success", "btn-warning").toClass() }
+        })
+    }
+
+    removeCard() {
+        this.setState(state => {
+            return {
+                cardCss: state.cardCss.split(' ').replaceClass('bounceIn', 'bounceOut').toClass()
+            }
         })
     }
 
     render() {
+        const { cardCss, addTaskClassName } = this.state
         return (
             <div className="container">
                 <h1>Hello World, from React App!!</h1>
@@ -35,15 +46,15 @@ class App extends React.Component {
                 <br />
                 <TodoItem title="Probando un todoItem" strikeIt={true} color={{ done: '#ed1c24', default: '#ccc' }} />
 
-                <div className="card bg-default animated bounceIn mt-5" style={{ width: 300 }}>
+                <div className={cardCss} style={{ width: 300 }}>
                     <div className="card-body">
                         <h5 className="card-title">Card title</h5>
                         <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                         <h6 className="card-subtitle text-muted float-right">Updated 2 min ago</h6>
                     </div>
                     <div className="card-footer">
-                        <a href="#" className={this.state.addTaskClassName.toClass()} onClick={this.addTask}>Add Task</a>
-                        <a href="#" className="btn btn-xs btn-danger float-right">Remove</a>
+                        <a href="#" className={addTaskClassName} onClick={this.addTask}>Add Task</a>
+                        <a href="#" className="btn btn-sm btn-danger float-right" onClick={this.removeCard}>Remove</a>
                     </div>
                 </div>
                 <pre>{this.state.addTaskClassName}</pre>

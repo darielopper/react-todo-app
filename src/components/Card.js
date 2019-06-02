a3ce9665ac5973a90cf9f866d5e678bf33c13eeb
dramosv90 <dariel.ramos.dv@gmail.com>
2019-06-02 19:09:47 -0400

    hiding card when remove it

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,7 @@ import { faClock } from "@fortawesome/free-solid-svg-icons"
 import TodoItem from "./TodoItem"
 const classnames = require('classnames')
 
-class Card extends React.Component{
+class Card extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
@@ -12,18 +12,24 @@ class Card extends React.Component{
             tasks: props.tasks
         }
         this.addTask = this.addTask.bind(this)
+        this.removeCard = this.removeCard.bind(this)
     }
 
     addTask() {
         this.setState(state => {
             let newTasks = [...state.tasks]
             newTasks.push({ title: 'New Task', strikeIt: true, editMode: true })
-            return {tasks: newTasks}
+            return { tasks: newTasks }
         })
     }
 
     removeCard() {
-
+        console.log('asd')
+        this.setState(state => {
+            const classAsText = state.cardCss.join(' ')
+            const replaceClass = classAsText.replace('bounceIn', 'bounceOut')
+            return { cardCss: replaceClass.split(' ') }
+        })
     }
 
     hasAnyTask() {
@@ -32,17 +38,18 @@ class Card extends React.Component{
 
     render() {
         const timeEllapsed = "Recently"
-        const {cardCss, tasks} = this.state
+        const { cardCss, tasks } = this.state
         return (
             <div className={classnames(cardCss)} style={{ width: 300 }}>
                 <div className="card-body">
                     <h5 className="card-title">{this.props.category}</h5>
                     <div className="card-text">
                         {this.hasAnyTask() && (
-                            tasks.map(item =>
+                            tasks.map((item, index) =>
                                 <TodoItem title={item.title}
                                     strikeIt={item.strikeIt}
                                     editMode={!!item.editMode}
+                                    key={item + '_' + index}
                                     onChanged={(val) => console.log(val)} />
                             )
                         )}
@@ -54,7 +61,8 @@ class Card extends React.Component{
                 <div className="card-footer">
                     <a href="javascript: void(0);" className={classnames('btn', 'btn-sm', 'btn-success')}
                         onClick={this.addTask}>Add Task</a>
-                    <a href="javascript: void(0);" className="btn btn-sm btn-danger float-right" onClick={() => this.removeCard}>Remove</a>
+                    <a href="javascript: void(0);" className="btn btn-sm btn-danger float-right"
+                        onClick={this.removeCard}>Remove</a>
                 </div>
             </div>
         )
